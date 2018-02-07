b9168ca78dce734ec4c6ba291a4100ce11d70a31
hschnegg <hschnegg@gmail.com>
2018-02-07 07:30:17 +0000

    Changed company-mode settings for cider

--- init.el
+++ init.el
@@ -86,7 +86,9 @@
 
 ;; Cider
 (require 'cider)
+(add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+(add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
 
 ;;;;;;;;;
