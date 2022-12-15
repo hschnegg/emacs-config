1981276e30872d9355bbd21a99009a4c04db9b97
Herve Schnegg <herveschnegg@Herves-MacBook-Pro.local>
2022-12-15 15:55:24 +0000

    Updates for new machine

--- init.el
+++ init.el
@@ -2,43 +2,30 @@
 ;; Emacs - Config ;;
 ;;;;;;;;;;;;;;;;;;;;
 
-;; Package initialisation
-(setq package-archives
-      '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
-        ("MELPA Stable" . "https://stable.melpa.org/packages/")
-        ("MELPA"        . "https://melpa.org/packages/"))
-      package-archive-priorities
-      '(("MELPA Stable" . 10)
-        ("GNU ELPA"     . 5)
-        ("MELPA"        . 0)))
 
-;; List of packages to install
-(defvar my/packages
-  '(exec-path-from-shell
-    magit
-    ess
-    elpy
-    cider
-    company
-    ;; markdown-mode
-    pyenv-mode
-    ;; haskell-mode
-    ;; tidal
-    ;; which-key
-    ))
+;; Install Missing Packages
+;; list the packages you want
+(setq package-list '(magit ess elpy cider company pyenv-mode which-key exec-path-from-shell))
 
-;; Install missing packages
-(require 'cl-lib)
-(defun my/install-packages ()
-  "Ensure the packages I use are installed. See `my/packages'."
-  (interactive)
-  (let ((missing-packages (cl-remove-if #'package-installed-p my/packages)))
-    (when missing-packages
-      (message "Installing %d missing package(s)" (length missing-packages))
-      (package-refresh-contents)
-      (mapc #'package-install missing-packages))))
+;; list the repositories containing them
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("MELPA Stable" . "https://stable.melpa.org/packages/")
+			 ("MELPA" . "https://melpa.org/packages/")))
 
-(my/install-packages)
+;; activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
 
 ;; Custome theme
 ;; curl -LO https://raw.githubusercontent.com/emacs-jp/replace-colorthemes/master/billw-theme.el
@@ -53,7 +40,7 @@
 (tool-bar-mode -1)
 
 ;; Avoid opening of new frame when opening from finder
-;; (setq ns-pop-up-frames nil)
+(setq ns-pop-up-frames nil)
 
 ;; Avoid custom data to be writen into init
 (setq custom-file (make-temp-file "custom.el"))
@@ -65,7 +52,7 @@
 (setq next-line-add-newlines t)
 
 ;; Allow hash sign
-(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+(global-set-key (kbd "M-3") #'(lambda () (interactive) (insert "#")))
 
 ;; Load path from shell
 (when (memq window-system '(mac ns))
@@ -76,13 +63,14 @@
 
 ;; which - key
 (require 'which-key)
+(which-key-mode)
 
 ;;;;;;;
 ;; R ;;
 ;;;;;;;
 
 ;; ESS
-(require 'ess-site)
+;; (require 'ess-site)
 
 ;;;;;;;;;;;;
 ;; Python ;;
@@ -92,6 +80,8 @@
 ;; pip install flake8
 ;; pip install jedi
 (elpy-enable)
+(setq elpy-rpc-virtualenv-path 'current)
+(pyenv-mode)
 
 ;;;;;;;;;;;;;
 ;; Clojure ;;
@@ -103,19 +93,6 @@
 (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
 (add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
-
-;;;;;;;;;;;;;;
-;; Markdown ;;
-;;;;;;;;;;;;;;
-
-(require 'markdown-mode)
-
-;;;;;;;;;;;;;;;;;;
-;; Tydal Cycles ;;
-;;;;;;;;;;;;;;;;;;
-
-;; (require 'tidal)
-;; (setq tidal-interpreter "~/.ghcup/bin/ghci")
 
 ;;;;;;;;;
 ;; End ;;
